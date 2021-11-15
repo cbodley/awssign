09faa44c7cb09f65442e09c2d890f23761a54ebf
Casey Bodley <cbodley@redhat.com>
2021-11-15 17:03:45 -0500

    v4: add canonical_request()
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

---
+++ include/awssign/v4/canonical_request.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <string_view>
+#include <awssign/detail/emit.hpp>
+#include <awssign/v4/canonical_headers.hpp>
+#include <awssign/v4/canonical_query.hpp>
+#include <awssign/v4/canonical_uri.hpp>
+
+namespace awssign::v4 {
+
+/// write the canonical request to output
+template <typename HeaderIterator,
+          typename Writer> // void(const char*, const char*)
+std::size_t canonical_request(std::string_view method,
+                              std::string_view uri_path,
+                              std::string_view query,
+                              HeaderIterator header0,
+                              HeaderIterator headerN,
+                              std::string_view payload_hash,
+                              Writer&& out)
+{
+  using awssign::detail::emit;
+  std::size_t bytes = 0;
+  // CanonicalRequest =
+  //   HTTPRequestMethod + '\n' +
+  bytes += emit(method.begin(), method.end(), out);
+  bytes += emit('\n', out);
+  //   CanonicalURI + '\n' +
+  canonical_uri(uri_path.begin(), uri_path.end(), out);
+  bytes += emit('\n', out);
+  //   CanonicalQueryString + '\n' +
+  canonical_query(query.begin(), query.end(), out);
+  bytes += emit('\n', out);
+  //   CanonicalHeaders + '\n' +
+  //   SignedHeaders + '\n' +
+  canonical_signed_headers(header0, headerN, out);
+  bytes += emit('\n', out);
+  //   HexEncode(Hash(RequestPayload))
+  bytes += emit(payload_hash.begin(), payload_hash.end(), out);
+  return bytes;
+}
+
+} // namespace awssign::v4
---
+++ test/test_v4_canonical_request.cc
@@ -0,0 +1,125 @@
+#include <awssign/v4/canonical_request.hpp>
+#include <gtest/gtest.h>
+
+namespace awssign {
+
+struct capture {
+  std::string& value;
+
+  template <typename Iterator> // forward iterator with value_type=char
+  void operator()(Iterator begin, Iterator end) {
+    value.append(begin, end);
+  }
+};
+
+struct header_type {
+  header_type(std::string_view name, std::string_view value) noexcept
+      : name_(name), value_(value)
+  {}
+  std::string_view name() const { return name_; }
+  std::string_view value() const { return value_; }
+ private:
+  std::string_view name_;
+  std::string_view value_;
+};
+
+// sha256sum of empty buffer
+static constexpr std::string_view empty_payload_hash =
+    "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
+
+TEST(canonical_request, aws4_testsuite_get_header_key_duplicate)
+{
+  const header_type headers[] = {
+    {"Host", "example.amazonaws.com"},
+    {"My-Header1", "value2"},
+    {"My-Header1", "value2"},
+    {"My-Header1", "value1"},
+    {"X-Amz-Date", "20150830T123600Z"},
+  };
+  std::string result;
+  v4::canonical_request("GET", "/", "", std::begin(headers),
+                        std::end(headers), empty_payload_hash,
+                        capture{result});
+  EXPECT_EQ(result, R"(GET
+/
+
+host:example.amazonaws.com
+my-header1:value2,value2,value1
+x-amz-date:20150830T123600Z
+
+host;my-header1;x-amz-date
+e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855)");
+}
+
+TEST(canonical_request, aws4_testsuite_get_header_value_multiline)
+{
+  const header_type headers[] = {
+    {"Host", "example.amazonaws.com"},
+    {"My-Header1", "value1\n  value2\n     value3"},
+    {"X-Amz-Date", "20150830T123600Z"},
+  };
+  std::string result;
+  v4::canonical_request("GET", "/", "", std::begin(headers),
+                        std::end(headers), empty_payload_hash,
+                        capture{result});
+  EXPECT_EQ(result, R"(GET
+/
+
+host:example.amazonaws.com
+my-header1:value1 value2 value3
+x-amz-date:20150830T123600Z
+
+host;my-header1;x-amz-date
+e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855)");
+}
+
+TEST(canonical_request, aws4_testsuite_get_header_value_order)
+{
+  const header_type headers[] = {
+    {"Host", "example.amazonaws.com"},
+    {"My-Header1", "value4"},
+    {"My-Header1", "value1"},
+    {"My-Header1", "value3"},
+    {"My-Header1", "value2"},
+    {"X-Amz-Date", "20150830T123600Z"},
+  };
+  std::string result;
+  v4::canonical_request("GET", "/", "", std::begin(headers),
+                        std::end(headers), empty_payload_hash,
+                        capture{result});
+  EXPECT_EQ(result, R"(GET
+/
+
+host:example.amazonaws.com
+my-header1:value4,value1,value3,value2
+x-amz-date:20150830T123600Z
+
+host;my-header1;x-amz-date
+e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855)");
+}
+
+TEST(canonical_request, aws4_testsuite_get_header_value_trim)
+{
+  const header_type headers[] = {
+    {"Host", "example.amazonaws.com"},
+    {"My-Header1", " value1"},
+    {"My-Header2", " \"a   b   c\""},
+    {"X-Amz-Date", "20150830T123600Z"},
+  };
+  std::string result;
+  v4::canonical_request("GET", "/", "", std::begin(headers),
+                        std::end(headers), empty_payload_hash,
+                        capture{result});
+  EXPECT_EQ(result, R"(GET
+/
+
+host:example.amazonaws.com
+my-header1:value1
+my-header2:"a b c"
+x-amz-date:20150830T123600Z
+
+host;my-header1;my-header2;x-amz-date
+e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855)");
+}
+
+} // namespace awssign
